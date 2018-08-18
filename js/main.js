b83b1648a17dda17e67c313c6ea6d2a9021e0465
Susanne <35730140+BettinaSDe@users.noreply.github.com>
2018-08-19 01:04:10 +0200

    image alt restaurant.name fix Lighthouse access. finding

--- js/main.js
+++ js/main.js
@@ -140,8 +140,8 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
-  image.alt = 'restaurant.name';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = restaurant.name;
   li.append(image);
 
   const name = document.createElement('h1');
